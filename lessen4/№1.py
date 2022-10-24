f3e996c84fcb6f25ee8d5f679e61c26274e1735b
legenda-dell <support@roolgenda.ru>
2022-10-24 15:52:18 +0300

    Update №1.py

--- lessen4/№1.py
+++ lessen4/№1.py
@@ -1,1 +1,11 @@
-a=int(input('Приввет'))
+n=int(input("Сколько рядов?"))
+r=0
+while r < n:
+    if r%2==0:
+        print(r*5+0)
+        print(r*5+1)
+        print(r*5+3)
+        print(r*5+4)
+    else:
+        print(r*5+2)
+    r=r+1
